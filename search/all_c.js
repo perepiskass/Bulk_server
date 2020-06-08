ffc4ed41c21878a7ec5f829ccc25d8b7626f2f16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-08 21:10:13 +0000

    Deploy perepiskass/Bulk_server to github.com/perepiskass/Bulk_server.git:gh-pages

--- search/all_c.js
+++ search/all_c.js
@@ -8,7 +8,7 @@ var searchData=
   ['set_5fcommandcount',['set_commandCount',['../classLogger.html#af947354159cfea459f988d3e570e46a6',1,'Logger']]],
   ['set_5flinecount',['set_lineCount',['../classLogger.html#afb488482288f08f750428c410d4c98ad',1,'Logger']]],
   ['setbulk',['setBulk',['../classObserver.html#a19f58696d77c49c546c6639bc7d9a8fa',1,'Observer::setBulk()'],['../classDataIn.html#a55c7ef4fa643fc3a4ce0c8ca824a43ad',1,'DataIn::setBulk()'],['../classDataToConsole.html#abfde2d8b1c97149c67636c29dfff438c',1,'DataToConsole::setBulk()'],['../classDataToFile.html#a952e881bfa20e744c765ef6e27fa11ce',1,'DataToFile::setBulk()']]],
-  ['setcommand',['setCommand',['../classHandler.html#a5ff7f15a05eb8efd488d3a1ba73ce75a',1,'Handler']]],
+  ['setcommand',['setCommand',['../classHandler.html#a1df938225c6342b26346d48e876f40e9',1,'Handler']]],
   ['setcommands',['setCommands',['../namespaceasync.html#afeaef81ba8080b05ec043f218bd369fc',1,'async']]],
   ['setcount',['setCount',['../classLogger.html#ae0689cfb3d7085d73cbae10eb493d389',1,'Logger']]],
   ['setdata',['setData',['../classDataIn.html#a46b7d0105f3e97597e0512a7e0f91056',1,'DataIn']]],
